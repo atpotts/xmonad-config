c2b48b2b8dbd9bd8ab618a90eecf2865041f8d38
Alistair Potts <alistairtpotts@gmail.com>
2017-09-23 20:46:27 +0100

    removed unreliable xmobar mouse support

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE
-  OverloadedStrings
+  OverloadedStrings,
+  TupleSections
 #-}
 
-import MyPrompts (myPromptKeyMap)
+-- My Personal Customizations
+import MyPrompts
 import Colors
 import WindowNames
 
+import Projects
+
+-- XMonad Modules
 import XMonad
 
 import XMonad.Hooks.DynamicLog
@@ -13,26 +18,37 @@ import XMonad.Util.Loggers
 
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run       (spawnPipe)
-import XMonad.Util.EZConfig  (additionalKeys)
+import XMonad.Util.EZConfig  (mkKeymap)
 
 import XMonad.Layout.Spacing (spacing)
 import XMonad.Layout.TwoPane
 
+import qualified XMonad.Layout.BoringWindows as B
+import XMonad.Layout.Minimize
+
+import qualified XMonad.Actions.DynamicWorkspaceOrder as DO
+import XMonad.Actions.CycleWS
 import XMonad.Actions.UpdatePointer
 
 import qualified XMonad.StackSet as W
 
+-- Ordinary Haskell Modules
+import Control.Monad (forM)
 import Data.Monoid           ((<>))
+import Data.Function((&))
+import Control.Arrow((>>>),(&&&),(***))
 import System.IO
-import Data.List (isInfixOf)
+import System.Exit (exitWith, ExitCode(ExitSuccess))
+import Data.List (isInfixOf,break)
+import Data.Char
 import qualified Data.Map as M
 
 -- Layout Names
-myLayout =  tall ||| twopane ||| wide ||| full
+myLayout = minimize (tall ||| twopane ||| wide ||| full)
   where 
     incstep = 3/100
     startdivision = 1/2
-    spc = spacing 10
+    spc = spacing 10 
     tall    = spc $ Tall 1 incstep startdivision
     twopane = spc $ TwoPane incstep startdivision
     wide    = spc $ Mirror tall
@@ -58,66 +74,181 @@ managementHooks = [
     ]
 
 main = do
-    xmproc <- runXmobar
-    xmonad $ docks defaultConfig {
+      xmproc <- runXmobar
+      xmonad $ dynamicProjects projects $ docks defaultConfig {
+          -- xmonad $ docks def {
           terminal   = "urxvt",
           manageHook = manageDocks <+> manageHook defaultConfig
                                    <+> composeAll managementHooks,
-          layoutHook = avoidStruts myLayout,
+          layoutHook = B.boringWindows $ avoidStruts myLayout,
           logHook    = xmobarHook xmproc,
           -- use Mod key
           modMask = cmdkey,
+          workspaces = ["scratch"],
+          keys = \conf -> mkKeymap conf $
+                myKeyMap "M-;" myXPConfig
+                      (myKeys conf
+                      ++ myPrompts myXPConfig windowNames
+                      ++ projectPrompts (mkColor myXPConfig blue)),
+
 
           -- Borders
           borderWidth = 3,
           normalBorderColor = base2,
           focusedBorderColor = base1
-          -- keys = myKeys
-        }
-        `additionalKeys` myKeys
+  }
 
 
-myKeys :: [((KeyMask, KeySym), X ())]
-myKeys = [
+myKeys :: XConfig Layout -> PromptList
+myKeys conf =
+    [ (["M-S-<Return>"], "Launch terminal", spawn $ XMonad.terminal conf)
+    , (["M-d"],       "Close window", kill)
 
-          ((cmdkey,                xK_d),      kill),                       
-          ((cmdkey,                xK_Return), sendMessage NextLayout),     
-          -- ((cmdkey .|. shiftMask , xK_Return), sendMessage PreviousLayout), 
+    , (["M-<Return>"],   "Next layout",  sendMessage NextLayout)
 
-          -- free xK_return
-          ((cmdkey,               xK_m), windows W.focusMaster),
-          ((cmdkey .|. shiftMask, xK_m), windows W.swapMaster)
+    , (["M-n"], "Reset window size", refresh)
 
+    -- Media keys
+    , (["<XF86AudioLowerVolume>"],"Vol-", spawn "amixer -D pulse set Master 2000-")
+    , (["<XF86AudioRaiseVolume>"], "Vol+", spawn "amixer -D pulse set Master 2000+")
+    , (["<XF86AudioMute>"], "Mute", spawn "amixer -D pulse set Master toggle")
 
-        ] ++ myPromptKeyMap cmdkey
+    -- , (["<XF86KbdBrightnessUp>"],"Keyboard Brightness Up",
+    --     spawn $  home ".nix-profile/bin/kbdlight" /./ "up")
+    -- , (["<XF86KbdBrightnessDown>"],"Keyboard Brightness Down",
+    --     spawn $  home ".nix-profile/bin/kbdlight" /./ "down")
 
+    , (["<XF86MonBrightnessUp>"],"Screen Brightness Up", spawn "xbacklight -inc +5")
+    , (["<XF86MonBrightnessDown>"],"Screen Brightness Down", spawn "xbacklight -inc -5")
+    -- move focus up or down the window stack
+    , (["M-j","M-<D>","M-<Tab>"],  "Next window", B.focusDown)
+    , (["M-k","M-<U>","M-S-<Tab>"],"Previous window",B.focusUp  )
+    , (["M-m"],                     "Go to master", B.focusMaster)
 
+    -- modifying the window order
+    , (["M-S-m"],           "Swap with master window", windows W.swapMaster)
+    , (["M-S-j","M-S-<D>"], "Swap window next",        windows W.swapDown  )
+    , (["M-S-k","M-S-<U>"], "Swap window previous",    windows W.swapUp    )
+    -- resizing the master/slave ratio
+    , (["M-S-=","M-="], "Expand master",    sendMessage Expand)
+    , (["M--"],         "Shrink master",    sendMessage Shrink)
 
+    -- floating layer support
+    , (["M-t"],"Unfloat",          withFocused $ windows . W.sink)
+
+    -- increase or decrease number of windows in the master area
+    , (["M-,"],"Increment master", sendMessage (IncMasterN 1))
+    , (["M-."],"Decrement master", sendMessage (IncMasterN (-1)))
+
+    -- quit, or restart
+    , (["M-S-<Backspace>"], "Quit xmonad", io (exitWith ExitSuccess))
+    , (["M-q"], "Restart xmonad",
+        spawn $ "if type xmonad;"
+              /./"then xmonad --recompile && xmonad --restart;"
+              /./"else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
+
+    -- Projects
+    , (["M-C-<L>","M-C-h"], "Send to previous project", DO.shiftTo  Prev HiddenNonEmptyWS)
+    , (["M-C-<R>","M-C-l"], "Send to next project",     DO.shiftTo  Next HiddenNonEmptyWS)
+    , (["M-<L>","M-h"],     "Previous project",         DO.moveTo   Prev HiddenNonEmptyWS)
+    , (["M-<R>","M-l"],     "Next project",             DO.moveTo   Next HiddenNonEmptyWS)
+    , (["M-S-<L>","M-S-h"], "Swap project Left",        DO.swapWith Prev HiddenNonEmptyWS)
+    , (["M-S-<R>","M-S-l"], "Swap project Right",       DO.swapWith Next HiddenNonEmptyWS)
+
+    , (["M-b"],   "Minimize",   withFocused minimizeWindow)
+    , (["M-S-b"], "Unminimize", sendMessage RestoreNextMinimizedWin)
+    , (["M-\\"], "Cycle within app", do
+        ws <- gets windowset
+        let p w1 w2 = do
+                w1g <- runQuery className w1
+                w2g <- runQuery className w2
+                return (w1g == w2g)
+        case W.stack $ W.workspace $ W.current ws of
+             Nothing -> return ()
+             Just x -> focusNext p x
+        )
+    ]
+    ++
+    [(["M-"++show i],"Show window "++show i,
+      do ws <- windowList <$> gets windowset
+         let w = take 1 $ drop (i-1) ws
+         case w of
+           [wn] -> myFocus wn
+           _    -> return ()
+     )| i <- [1..9]]
+     ++
+    [(["M-S-"++show i],"Move window to "++show i,
+                    windows $ W.modify' (myMoveTo i)
+     )| i <- [1..9]]
+
+myFocus :: Window -> X ()
+myFocus w = focus w >> (sendMessage $ RestoreMinimizedWin w)
+
+focusNext :: (Window -> Window -> X (Bool)) -> W.Stack Window -> X ()
+focusNext p s@W.Stack{W.up=up, W.down=down, W.focus=fs} = do
+    applist <- sequence $ map (\x -> (,x) <$> p fs x) (down ++ reverse up)
+    case map snd $ filter (fst) applist of
+         []    -> return () 
+         (x:_) -> myFocus  x
 runXmobar = spawnPipe $  home ".nix-profile/bin/xmobar"
-                     /./ home ".xmobarrc"
+                     /./ home ".xmonad/xmobarrc"
+
+
+myMoveTo :: Int -> W.Stack Window -> W.Stack Window
+myMoveTo n s@W.Stack{W.up=up,W.down=down} =
+    let (newup,newdown) = splitAt (n-1) $ reverse up ++ down
+    in s{W.up=reverse newup,W.down=newdown}
+
+
+windowList :: WindowSet -> [Window]
+windowList = W.current 
+         >>> W.workspace
+         >>> W.stack
+         >>> fmap W.integrate 
+         >>> foldr const []
+
+
+myOpenWindows :: Logger
+myOpenWindows = fmap Just $ withWindowSet $ \a ->
+   fmap concat $ forM (zip [1..] $ windowList a) $ \(i,w) -> do
+          g <- runQuery className w
+          let outcolor
+                  | Just w == W.peek a = xmobarColor (accentcolors g) base3 
+                  | otherwise          = xmobarColor (accentcolors g) base2 
+
+              pad | i `mod` 3 == 0 = " "
+                  | otherwise      = ""
+          return . (++pad). outcolor $ take 1 g
 
 myLogTitle :: Logger
-myLogTitle = withWindowSet $ traverse (anyWindowNamer Nothing) . W.peek
+myLogTitle = withWindowSet
+           $ traverse (anyWindowNamer Nothing
+                         (\a -> xmobarColor (accentcolors a) inherit a) id)
+           . W.peek
+
 
 xmobarHook xmproc = dynamicLogWithPP xmobarPP
-                          { ppOutput          = hPutStrLn xmproc
-                          , ppCurrent         = \n ->
-                                xmobarColor base3 (accentcolors n) $ pad n
-                          , ppVisible         = xmobarColor base0 inherit .
-                                (\x -> "["<> x <>"]")
-                          , ppHidden          = xmobarColor base0 inherit . pad
-                          , ppHiddenNoWindows = const ""
-                          , ppLayout = xmobarColor base2 base1 . layoutformatter
-                          , ppTitle = const ""
-                          , ppExtras = [
-                                fmap (fmap
-                                        (xmobarColor base0 base2 . shorten 50))
-                                     myLogTitle
-                                ]
-                          , ppSep = " "
-                          , ppWsSep = ""
-                          , ppOrder = \(layout : workspaces : title : rest) ->
-                                        workspaces : layout : rest
-                          }
+  { ppOutput  = hPutStrLn xmproc
+  , ppCurrent = \n ->
+        xmobarColor base3 (accentcolors n) $ pad $ pad n
+  , ppVisible = \x ->
+        xmobarColor base0 inherit $ "["<> x <>"]"
+  , ppHidden  = \x -> take 1 x
+                   & pad
+                   & xmobarColor base2 (accentcolors x)
+  , ppHiddenNoWindows = const ""
+  , ppLayout = xmobarColor base2 base1 . layoutformatter
+  , ppTitle = const ""
+  , ppExtras = [
+        fmap (fmap (\a -> "["++trim a++"]"))  myOpenWindows,
+        fmap (fmap (xmobarColor base0 base2 . shorten 90)) myLogTitle
+        ]
+  , ppSep = " "
+  , ppWsSep = ""
+  , ppOrder = \(layout : workspaces : title : rest) ->
+                workspaces : layout : rest
+  , ppSort = DO.getSortByOrder
+  }
 
-home x = "/home/alistairtpotts/" <> x
+home x = "/home/alistairtpotts/" ++ x
+
