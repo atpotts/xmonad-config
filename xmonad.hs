58a7b7d5b1d9b8aacdc6d8f778dac1c7d0a7061d
Alistair Potts <alistairtpotts@gmail.com>
2017-12-10 17:59:31 +0000

    tidy up

--- xmonad.hs
+++ xmonad.hs
@@ -34,6 +34,11 @@ import qualified XMonad.Actions.DynamicWorkspaceOrder as DO
 import XMonad.Actions.CycleWS
 import XMonad.Actions.UpdatePointer
 
+import qualified XMonad.Layout.Groups as Gr 
+import qualified XMonad.Layout.Groups.Helpers as Gh
+import XMonad.Layout.Tabbed 
+import TabGroups
+
 import qualified XMonad.StackSet as W
 
 -- Ordinary Haskell Modules
@@ -48,17 +53,31 @@ import Data.Char
 import qualified Data.Map as M
 
 -- Layout Names
-myLayout = mkToggle (single MIRROR) $ minimize (tall ||| twopane ||| full)
-  where 
-    incstep = 3/100
-    startdivision = 1/2
-    spc = spacing 10 
-    tall    = spc $ Tall 1 incstep startdivision
-    twopane = spc $ TwoPane incstep startdivision
-    full    = spc $ Full
+-- myLayout = mkToggle (single MIRROR) $ minimize (tall ||| twopane ||| full)
+--   where 
+--     incstep = 3/100
+--     startdivision = 1/2
+--     spc = spacing 10 
+--     tall    = spc $ Tall 1 incstep startdivision
+--     twopane = spc $ TwoPane incstep startdivision
+--     full    = spc $ Full
 
-isMirror = isInfixOf "Mirror"
-layoutformatter s
+myLayout = minimize $ tallTabs $ def { tabsTheme = def {
+         activeColor = yellow,
+         inactiveColor = base2,
+         urgentColor = magenta,
+         activeBorderColor = yellow,
+         inactiveBorderColor = base3,
+         urgentBorderColor = base3,
+         urgentTextColor = base02,
+         activeTextColor = base02,
+         inactiveTextColor = base02,
+         fontName = myFont,
+         decoHeight = 36
+}}
+
+isMirror x = isInfixOf "Mirror" x || isInfixOf "Horizontal" x
+layoutformatter s 
     | "Full" `isInfixOf` s   = "   "
     | "TwoPane" `isInfixOf` s =
         if isMirror s then "━━━" else " ┃ "
@@ -76,15 +95,18 @@ a /&/ b = a <> " && " <> b
 
 managementHooks :: [ManageHook]
 managementHooks = [
-    resource =? "stalonetray" --> doIgnore
+    resource =? "stalonetray" --> doIgnore,
+    className =? "Xmessage" --> doFloat,
+    resource =? "Dialog" --> doFloat
     ]
 
 main = do
       xmproc <- runXmobar
       spawn "offlineimap"
+      spawn $ home ".fehbg"
       xmonad $ dynamicProjects projects $ docks defaultConfig {
           -- xmonad $ docks def {
-          terminal   = "urxvt",
+          terminal   = "urxvt -e zsh",
           manageHook = manageDocks <+> manageHook defaultConfig
                                    <+> composeAll managementHooks,
           layoutHook = B.boringWindows $ avoidStruts myLayout,
@@ -101,8 +123,8 @@ main = do
 
           -- Borders
           borderWidth = 3,
-          normalBorderColor = base2,
-          focusedBorderColor = base1
+          normalBorderColor = base3,
+          focusedBorderColor = yellow
   }
 
 
@@ -111,15 +133,29 @@ myKeys conf =
     [ (["M-S-<Return>"], "Launch terminal", spawn $ XMonad.terminal conf)
     , (["M-d"],       "Close window", kill)
 
-    , (["M-<Return>"],   "Next layout",  sendMessage NextLayout)
-    , (["M-\\"], "Toggle Mirror", sendMessage $ Toggle MIRROR)
+    , (["M-<Return>"],   "Next layout",  nextOuterLayout)
+    -- , (["M-\\"], "Toggle Mirror", sendMessage $ Toggle MIRROR)
+    , (["M-<Tab>"], "Cycle Group", sendMessage $ Gr.Modify Gr.focusDown)
+    , (["M-S-<Tab>"], "Cycle Backwards", sendMessage $ Gr.Modify Gr.focusUp)
+    , (["M-\\"], "Move Through Group", 
+                  sendMessage (Gr.Modify Gr.swapDown))
+    , (["M-S-\\"], "Move Through Group", sendMessage $ Gr.Modify Gr.swapMaster)
+    , (["M-m"], "Move Through Group", sendMessage $ Gr.Modify Gr.focusMaster)
+
+    , (["M-S-]"], "Move Through Group", sendMessage $ Gr.Modify Gr.moveToNewGroupDown)
+    , (["M-S-["], "Move Through Group", sendMessage $ Gr.Modify Gr.moveToNewGroupUp)
+    , (["M-["],"Move To PreviousGroup", sendMessage $ Gr.Modify $ Gr.moveToGroupUp True)
+      , (["M-]"],"Move to Next Group", sendMessage $ Gr.Modify $ Gr.moveToGroupDown True)
 
     , (["M-n"], "Reset window size", refresh)
 
     -- move focus up or down the window stack
-    , (["M-j","M-<D>","M-<Tab>"],  "Next window", B.focusDown)
-    , (["M-k","M-<U>","M-S-<Tab>"],"Previous window",B.focusUp  )
-    , (["M-m"],                     "Go to master", B.focusMaster)
+    -- , (["M-j","M-<D>","M-<Tab>"],  "Next window", B.focusDown)
+    -- , (["M-k","M-<U>","M-S-<Tab>"],"Previous window",B.focusUp  )
+    -- , (["M-m"],                     "Go to master", B.focusMaster)
+    , (["M-j","M-<D>"],  "Next window", sendMessage $ Gr.Modify Gr.focusGroupDown)
+    , (["M-k","M-<U>"],"Previous window",sendMessage $ Gr.Modify Gr.focusGroupUp  )
+    , (["M-m"],                     "Go to master", sendMessage $ Gr.Modify Gr.focusGroupMaster)
 
     --media keys
     , (["<XF86AudioLowerVolume>"],"Vol-", spawn "amixer set Master 2000-")
@@ -135,19 +171,26 @@ myKeys conf =
     , (["<XF86MonBrightnessDown>"],"Screen Brightness Down", spawn "xbacklight -inc -5")
     
     -- modifying the window order
-    , (["M-S-m"],           "Swap with master window", windows W.swapMaster)
-    , (["M-S-j","M-S-<D>"], "Swap window next",        windows W.swapDown  )
-    , (["M-S-k","M-S-<U>"], "Swap window previous",    windows W.swapUp    )
+    -- , (["M-S-m"],           "Swap with master window", windows W.swapMaster)
+    -- , (["M-S-j","M-S-<D>"], "Swap window next",        windows W.swapDown  )
+    -- , (["M-S-k","M-S-<U>"], "Swap window previous",    windows W.swapUp    )
+    , (["M-S-m"],           "Swap with master group",
+                sendMessage $ Gr.Modify Gr.swapGroupMaster)
+    , (["M-S-j","M-S-<D>"], "Swap window next",
+                sendMessage $ Gr.Modify Gr.swapGroupUp)
+    , (["M-S-k","M-S-<U>"], "Swap window previous",
+                sendMessage $ Gr.Modify Gr.swapGroupDown)
+
     -- resizing the master/slave ratio
-    , (["M-S-=","M-="], "Expand master",    sendMessage Expand)
-    , (["M--"],         "Shrink master",    sendMessage Shrink)
+    , (["M-S-=","M-="], "Expand master",    expandMasterGroups)
+    , (["M--"],         "Shrink master",    shrinkMasterGroups)
 
     -- floating layer support
     , (["M-t"],"Unfloat",          withFocused $ windows . W.sink)
 
     -- increase or decrease number of windows in the master area
-    , (["M-,"],"Increment master", sendMessage (IncMasterN 1))
-    , (["M-."],"Decrement master", sendMessage (IncMasterN (-1)))
+    , (["M-,"],"Increment master", increaseNMasterGroups )
+    , (["M-."],"Decrement master", decreaseNMasterGroups)
 
     -- quit, or restart
     , (["M-S-<Backspace>"], "Quit xmonad", io (exitWith ExitSuccess))
@@ -179,7 +222,7 @@ myKeys conf =
 
     , (["M-b"],   "Minimize",   withFocused minimizeWindow)
     , (["M-S-b"], "Unminimize", sendMessage RestoreNextMinimizedWin)
-    , (["M-<"], "Cycle within app", do
+    , (["M-S-,","M-<"], "Cycle within app", do
         ws <- gets windowset
         let p w1 w2 = do
                 w1g <- runQuery className w1
@@ -192,18 +235,18 @@ myKeys conf =
     ]
     ++
     systemKeys
-    ++
-    [(["M-"++show i],"Show window "++show i,
-      do ws <- windowList <$> gets windowset
-         let w = take 1 $ drop (i-1) ws
-         case w of
-           [wn] -> myFocus wn
-           _    -> return ()
-     )| i <- [1..9]]
-     ++
-    [(["M-S-"++show i],"Move window to "++show i,
-                    windows $ W.modify' (myMoveTo i)
-     )| i <- [1..9]]
+    -- ++
+    -- [(["M-"++show i],"Show window "++show i,
+    --   do ws <- windowList <$> gets windowset
+    --      let w = take 1 $ drop (i-1) ws
+    --      case w of
+    --        [wn] -> myFocus wn
+    --        _    -> return ()
+    --  )| i <- [1..9]]
+    --  ++
+    -- [(["M-S-"++show i],"Move window to "++show i,
+    --                 windows $ W.modify' (myMoveTo i)
+    --  )| i <- [1..9]]
 
 myFocus :: Window -> X ()
 myFocus w = focus w >> (sendMessage $ RestoreMinimizedWin w)
@@ -219,10 +262,10 @@ runXmobar = spawnPipe $  home ".nix-profile/bin/xmobar"
                      /./ home (".xmonad/xmobarrc-"++colorscheme)
 
 
-myMoveTo :: Int -> W.Stack Window -> W.Stack Window
-myMoveTo n s@W.Stack{W.up=up,W.down=down} =
-    let (newup,newdown) = splitAt (n-1) $ reverse up ++ down
-    in s{W.up=reverse newup,W.down=newdown}
+-- myMoveTo :: Int -> W.Stack Window -> W.Stack Window
+-- myMoveTo n s@W.Stack{W.up=up,W.down=down} =
+--     let (newup,newdown) = splitAt (n-1) $ reverse up ++ down
+--     in s{W.up=reverse newup,W.down=newdown}
 
 
 windowList :: WindowSet -> [Window]
@@ -233,17 +276,6 @@ windowList = W.current
          >>> foldr const []
 
 
-myOpenWindows :: Logger
-myOpenWindows = fmap Just $ withWindowSet $ \a ->
-   fmap concat $ forM (zip [1..] $ windowList a) $ \(i,w) -> do
-          g <- runQuery className w
-          let outcolor
-                  | Just w == W.peek a = xmobarColor (accentcolors g) base3 
-                  | otherwise          = xmobarColor (accentcolors g) base2 
-
-              pad | i `mod` 3 == 0 = " "
-                  | otherwise      = ""
-          return . (++pad). outcolor $ take 1 g
 
 myLogTitle :: Logger
 myLogTitle = withWindowSet
@@ -265,7 +297,6 @@ xmobarHook xmproc = dynamicLogWithPP xmobarPP
   , ppLayout = xmobarColor base2 base1 . layoutformatter
   , ppTitle = const ""
   , ppExtras = [
-        fmap (fmap (\a -> "["++trim a++"]"))  myOpenWindows,
         fmap (fmap (xmobarColor base0 base2 . shorten 90)) myLogTitle
         ]
   , ppSep = " "
